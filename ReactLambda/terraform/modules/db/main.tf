c6122fdf4360f2c8b1cbe735c9745ddcabd0fb3e
prokschf <proksch.florian@googlemail.com>
2023-01-09 10:04:56 +0100

    'terra'

--- ReactLambda/terraform/modules/db/main.tf
+++ ReactLambda/terraform/modules/db/main.tf
@@ -6,29 +6,16 @@ variable "stage_name" {
   type = string
 }
 
-resource "aws_dynamodb_table" "realworld-users-table" {
-  name           = "realworld-${var.stage_name}-users"
+resource "aws_dynamodb_table" "realworld-user-table" {
+  name           = "realworld-${var.stage_name}-user"
   billing_mode   = "PROVISIONED"
   read_capacity  = 1
   write_capacity = 1
-  hash_key       = "username"
+  hash_key       = "Username"
 
   attribute {
-    name = "username"
+    name = "Username"
     type = "S"
-  }
-
-  attribute {
-    name = "email"
-    type = "S"
-  }
-
-  global_secondary_index {
-    name               = "email"
-    hash_key           = "email"
-    write_capacity     = 1
-    read_capacity      = 1
-    projection_type    = "ALL"
   }
 
   tags = {
@@ -36,66 +23,231 @@ resource "aws_dynamodb_table" "realworld-users-table" {
   }
 }
 
-resource "aws_dynamodb_table" "realworld-articles-table" {
-  name           = "realworld-${var.stage_name}-articles"
+resource "aws_dynamodb_table" "realworld-email-user-table" {
+  name           = "realworld-${var.stage_name}-email-user"
   billing_mode   = "PROVISIONED"
   read_capacity  = 1
   write_capacity = 1
-  hash_key       = "slug"
+  hash_key       = "Email"
 
   attribute {
-    name = "slug"
+    name = "Email"
+    type = "S"
+  }
+
+  tags = {
+    Environment = "${var.stage_name}"
+  }
+}
+
+resource "aws_dynamodb_table" "realworld-follow-table" {
+  name           = "realworld-${var.stage_name}-follow"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "Follower"
+  range_key       = "Publisher"
+
+  attribute {
+    name = "Follower"
     type = "S"
   }
 
   attribute {
-    name = "dummy"
+    name = "Publisher"
+    type = "S"
+  }
+
+  tags = {
+    Environment = "${var.stage_name}"
+  }
+}
+
+resource "aws_dynamodb_table" "realworld-article-table" {
+  name           = "realworld-${var.stage_name}-article"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "ArticleId"
+
+  attribute {
+    name = "ArticleId"
+    type = "N"
+  }
+
+  attribute {
+    name = "CreatedAt"
+    type = "N"
+  }
+
+  attribute {
+    name = "Dummy"
+    type = "N"
+  }
+
+  attribute {
+    name = "Author"
+    type = "S"
+  }
+
+  global_secondary_index {
+    name               = "CreatedAt"
+    hash_key           = "Dummy"
+    range_key          = "CreatedAt"
+    write_capacity     = 1
+    read_capacity      = 1
+    projection_type    = "ALL"
+  }  
+
+  global_secondary_index {
+    name               = "Author"
+    hash_key           = "Author"
+    range_key          = "CreatedAt"
+    write_capacity     = 1
+    read_capacity      = 1
+    projection_type    = "ALL"
+  }    
+
+  tags = {
+    Environment = "${var.stage_name}"
+  }
+}
+
+resource "aws_dynamodb_table" "realworld-article-tag-table" {
+  name           = "realworld-${var.stage_name}-article-tag"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "Tag"
+  range_key       = "ArticleId"
+
+  attribute {
+    name = "Tag"
     type = "S"
   }
 
   attribute {
-    name = "updatedAt"
+    name = "ArticleId"
+    type = "N"
+  }
+
+  attribute {
+    name = "CreatedAt"
+    type = "N"
+  }
+
+  local_secondary_index {
+    name               = "CreatedAt"
+    #hash_key           = "Tag"
+    range_key          = "CreatedAt"
+    projection_type    = "ALL"
+  }  
+
+  tags = {
+    Environment = "${var.stage_name}"
+  }
+}
+
+resource "aws_dynamodb_table" "realworld-tag-table" {
+  name           = "realworld-${var.stage_name}-tag"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "Tag"
+
+  attribute {
+    name = "Tag"
+    type = "S"
+  }
+
+  attribute {
+    name = "ArticleCount"
+    type = "N"
+  }
+
+  attribute {
+    name = "Dummy"
     type = "N"
   }
 
   global_secondary_index {
-    name               = "updatedAt"
-    hash_key           = "dummy"
-    range_key          = "updatedAt"
+    name               = "ArticleCount"
+    hash_key           = "Dummy"
+    range_key          = "ArticleCount"
     write_capacity     = 1
     read_capacity      = 1
     projection_type    = "ALL"
-  }
+  }  
 
   tags = {
     Environment = "${var.stage_name}"
   }
 }
 
-resource "aws_dynamodb_table" "realworld-comments-table" {
-  name           = "realworld-${var.stage_name}-comments"
+resource "aws_dynamodb_table" "realworld-favorite-article-table" {
+  name           = "realworld-${var.stage_name}-favorite-article"
   billing_mode   = "PROVISIONED"
   read_capacity  = 1
   write_capacity = 1
-  hash_key       = "id"
+  hash_key       = "Username"
+  range_key       = "ArticleId"
 
   attribute {
-    name = "id"
+    name = "Username"
     type = "S"
   }
 
   attribute {
-    name = "slug"
-    type = "S"
+    name = "ArticleId"
+    type = "N"
   }
 
-  global_secondary_index {
-    name               = "article"
-    hash_key           = "slug"
-    write_capacity     = 1
-    read_capacity      = 1
+  attribute {
+    name = "FavoritedAt"
+    type = "N"
+  }
+
+  local_secondary_index {
+    name               = "FavoritedAt"
+    #hash_key           = "Username"
+    range_key          = "FavoritedAt"
     projection_type    = "ALL"
+  }  
+
+  tags = {
+    Environment = "${var.stage_name}"
   }
+}
+
+resource "aws_dynamodb_table" "realworld-comment-table" {
+  name           = "realworld-${var.stage_name}-comment"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "ArticleId"
+  range_key       = "CommentId"
+
+  attribute {
+    name = "ArticleId"
+    type = "N"
+  }
+
+  attribute {
+    name = "CommentId"
+    type = "N"
+  }
+
+  attribute {
+    name = "CreatedAt"
+    type = "N"
+  }
+
+  local_secondary_index {
+    name               = "CreatedAt"
+    #hash_key           = "ArticleId"
+    range_key          = "CreatedAt"
+    projection_type    = "ALL"
+  }  
 
   tags = {
     Environment = "${var.stage_name}"
