69b7a0bd69d6d74bb43ffa9d48e661327ada6125
prokschf <proksch.florian@googlemail.com>
2023-01-10 01:59:37 +0100

    gw logs

--- terraform/modules/services/backend/main.apigw.tf
+++ terraform/modules/services/backend/main.apigw.tf
@@ -2,7 +2,60 @@ resource "aws_api_gateway_rest_api" "backend_gw" {
   name = "realworld-backend-${var.stage_name}"
 }
 
+resource "aws_api_gateway_account" "gw_account" {
+  cloudwatch_role_arn = aws_iam_role.cloudwatch.arn
+}
 
+resource "aws_iam_role" "cloudwatch" {
+  name = "api_gateway_cloudwatch_global"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "apigateway.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "cloudwatch" {
+  name = "default"
+  role = aws_iam_role.cloudwatch.id
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "logs:CreateLogGroup",
+                "logs:CreateLogStream",
+                "logs:DescribeLogGroups",
+                "logs:DescribeLogStreams",
+                "logs:PutLogEvents",
+                "logs:GetLogEvents",
+                "logs:FilterLogEvents"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_cloudwatch_log_group" "cw_loggroup" {
+  name              = "API-Gateway-Execution-Logs_${aws_api_gateway_rest_api.backend_gw.id}/${var.stage_name}"
+  retention_in_days = 90
+}
 
 resource "aws_api_gateway_resource" "api_resource" {
   parent_id   = aws_api_gateway_rest_api.backend_gw.root_resource_id
